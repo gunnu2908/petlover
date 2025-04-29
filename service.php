c33a5c2a403ed3ee1f47e5be76b825ca852b2cf4
Gunnu <gunnuchawla789@gmail.com>
2025-04-29 19:46:33 +0530

    removed testo

--- service.php
+++ service.php
@@ -159,22 +159,22 @@ $cart_count = $row['count'];
             </div>
            
             <div class="row pb-3">
+
             <?php
-$con=mysqli_connect("localhost","root","","petcarehub")or die (mysqli_error());
-$data="select * from adminservice";
-$result=mysqli_query($con,$data);
-while($row=$result->fetch_assoc())
-{
-$a=$row['CurrentDate'];
-$b=$row['ServiceCode'];
-$c=$row['ServiceName'];
-$d=$row['ServiceDescription'];
-$e=$row['ServicePhoto1'];
-$f=$row['Photo2'];
-$g=$row['Sdescr'];
+                $con=mysqli_connect("localhost","root","","petcarehub")or die (mysqli_error());
+                $data="select * from adminservice";
+                $result=mysqli_query($con,$data);
+                while($row=$result->fetch_assoc())
+                {
+                $a=$row['CurrentDate'];
+                $b=$row['ServiceCode'];
+                $c=$row['ServiceName'];
+                $d=$row['ServiceDescription'];
+                $e=$row['ServicePhoto1'];
+                $f=$row['Photo2'];
+                $g=$row['Sdescr'];
+                ?>
 
-
-?>
                 <div class="col-md-6 col-lg-4 mb-4">
                     <div class="d-flex flex-column text-center bg-white mb-2 p-3 p-sm-5">
                         <h3 class="flaticon-house display-3 font-weight-normal text-secondary mb-3"></h3>
@@ -208,50 +208,6 @@ mysqli_query($con,$data);
  echo "<script>location.href='singleservice.php';</script>";
  }
 ?>
-
-    <!-- Testimonial Start -->
-    <div class="container-fluid p-0 py-5">
-        <div class="container p-0 pt-5">
-            <div class="d-flex flex-column text-center mb-5">
-                <h4 class="text-secondary mb-3">Testimonial</h4>
-                <h1 class="display-4 m-0">Our Client <span class="text-primary">Says</span></h1>
-            </div>
-            <div class="owl-carousel testimonial-carousel">
-                <div class="bg-light mx-3 p-4">
-                    <div class="d-flex align-items-end mb-3 mt-n4 ml-n4">
-                        <img class="img-fluid" src="img/testimonial-1.jpg" style="width: 80px; height: 80px;" alt="">
-                        <div class="ml-3">
-                            <h5>Client Name</h5>
-                            <i>Profession</i>
-                        </div>
-                    </div>
-                    <p class="m-0">Sed ea amet kasd elitr stet, stet rebum et ipsum est duo elitr eirmod clita lorem. Dolor tempor ipsum sanct clita</p>
-                </div>
-               
-                <div class="bg-light mx-3 p-4">
-                    <div class="d-flex align-items-end mb-3 mt-n4 ml-n4">
-                        <img class="img-fluid" src="img/testimonial-3.jpg" style="width: 80px; height: 80px;" alt="">
-                        <div class="ml-3">
-                            <h5>Client Name</h5>
-                            <i>Profession</i>
-                        </div>
-                    </div>
-                    <p class="m-0">Sed ea amet kasd elitr stet, stet rebum et ipsum est duo elitr eirmod clita lorem. Dolor tempor ipsum sanct clita</p>
-                </div>
-                <div class="bg-light mx-3 p-4">
-                    <div class="d-flex align-items-end mb-3 mt-n4 ml-n4">
-                        <img class="img-fluid" src="img/testimonial-4.jpg" style="width: 80px; height: 80px;" alt="">
-                        <div class="ml-3">
-                            <h5>Client Name</h5>
-                            <i>Profession</i>
-                        </div>
-                    </div>
-                    <p class="m-0">Sed ea amet kasd elitr stet, stet rebum et ipsum est duo elitr eirmod clita lorem. Dolor tempor ipsum sanct clita</p>
-                </div>
-            </div>
-        </div>
-    </div>
-    <!-- Testimonial End -->
 
 
     <!-- Footer Start -->
